c87151cc9d5b29c1d5d52807f48c35edf437250d
kjbin0420 <kjbin0420@naver.com>
2020-09-20 21:30:01 +0900

    set spring security configuration

--- src/main/java/com/kjbin0420/oauth/OauthApplication.java
+++ src/main/java/com/kjbin0420/oauth/OauthApplication.java
@@ -2,12 +2,16 @@ package com.kjbin0420.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class OauthApplication {
+    private final String PROPERTIES = "spring.config.location=classpath:/google.yml";
 
     public static void main(String[] args) {
-        SpringApplication.run(OauthApplication.class, args);
+        new SpringApplicationBuilder(OauthApplication.class)
+                .properties()
+                .run(args);
     }
 
 }
---
+++ src/main/java/com/kjbin0420/oauth/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.kjbin0420.oauth.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/h2-console/**", "/favicon.ico").permitAll()
+                .anyRequest().authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll()
+                .and().headers().frameOptions().sameOrigin()
+                .and().csrf().disable();
+    }
+}
